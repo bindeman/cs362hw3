2271a77288fb8b60e6e600cbb7e95c50a634c4dd
Phillip Bindeman <bin@me.com>
2021-02-06 22:30:11 -0800

    Hw 4

---
+++ hw4/hw4.py
@@ -0,0 +1,13 @@
+#Phillip Bindeman
+# CS 362 In Class Activity
+# Homework 3
+
+
+def cube(a):
+    return abs(a)*a*a
+    
+def fullname(first, last):
+    return first, " ", last
+    
+def findAvgOfList(a):
+return sum(a) / len(a)
