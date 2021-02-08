f99d7365edfb323456943e4582d10d680a863050
Phillip Bindeman <bin@me.com>
2021-02-07 21:18:31 -0800

    Add changes

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -4,10 +4,13 @@
 
 
 def cube(a):
+    if(type(a) is not (int or float)) return "Invalid input"
+    #make the first value an absolute value to ensure it is positive
     return abs(a)*a*a
     
 def fullname(first, last):
     return first, " ", last
     
 def findAvgOfList(a):
+if(len(a) == 0) return "Cannot find avg of empty list"
 return sum(a) / len(a)
--- hw4/testHw4.py
+++ hw4/testHw4.py
@@ -5,23 +5,26 @@ import hw4
 
 class TestCalculator(unittest.TestCase):
     def testCubing(self):
-        self.assertEqual(hw3.cube(2), 8)
-        self.assertEqual(hw3.cube(-7), 147)
-        self.assertEqual(hw3.cube(123.4), 1879080.904)
+        self.assertEqual(hw4.cube(2), 8)
+        self.assertEqual(hw4.cube(-7), 147)
+        self.assertEqual(hw4.cube("3"), "Invalid input")
+        self.assertEqual(hw4.cube(123.4), 1879080.904)
+        self.assertEqual(hw4.cube(sqrt(2)), 1879080.904)
 
 
-        
     def testFindingAvgOfList(self):
-        self.assertEqual(hw3.fullname("Phil", "Bindeman"), "Phillip Bindeman")
-        self.assertEqual(hw3.fullname("Hi ", "There"), "Hi  There")
-        self.assertEqual(hw3.fullname(123, 332), "123 332")
-
-        
+        self.assertEqual(hw4.findAvgOfList([2, 3, 1, 5, 1]), 2.4)
+        self.assertEqual(hw4.findAvgOfList([2.4, 3.1, 5.123, 9.12, 4512.12]), 906.3726)
+        self.assertEqual(hw4.findAvgOfList([2]), 2)
+        self.assertEqual(hw4.findAvgOfList([]), "Cannot find avg of empty list")
+        self.assertRaises(TypeError, cs362question2.findAvgOfList, ["7", "2", 2])
+    
+    
     def testFullname(self):
-        self.assertEqual(hw3.findAvgOfList([2, 3, 1, 5, 1]), 2.4)
-        self.assertEqual(hw3.findAvgOfList([2.4, 3.1, 5.123, 9.12, 4512.12]), 906.3726)
-        self.assertEqual(hw3.findAvgOfList([2]), 2)
-        self.assertEqual(hw3.findAvgOfList(["7", "2", 2]), "Uncaught Exception")
+        self.assertEqual(hw4.fullname("Phil", "Bindeman"), "Phillip Bindeman")
+        self.assertEqual(hw4.fullname("Hi ", "There"), "Hi  There")
+        self.assertEqual(hw4.fullname("", 332), "332")
+        self.assertEqual(hw4.fullname("Phil", 332), "Phil 332")
 
 
 if __name__ == '__main__':
