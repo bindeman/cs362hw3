8f73d12b68bf2951b9461914bdc4f9df0be00a9e
Phillip Bindeman <bin@me.com>
2021-03-05 00:23:56 -0800

    (PROBLEM 2 START) Implemented 1st test case of a year being a leap divisible by 4

---
+++ hw7/leapYear.py
@@ -0,0 +1,6 @@
+#Phillip Bindeman
+# CS 362, Winter 2021
+# Problem 2, Homework 7
+
+def leapYear(n):
+    return pass
---
+++ hw7/test_LeapYear.py
@@ -0,0 +1,14 @@
+#Phillip Bindeman
+# CS 362, Winter 2021
+# Problem 2, Homework 7
+
+import unittest
+import fizzBuzz
+
+class HW7TestCases(unittest.TestCase):
+    def testleapYear(self):
+        self.assertEqual(leapYear.leapYear(8), True)
+
+
+
+
